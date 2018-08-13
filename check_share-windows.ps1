b6435f3108e324151e48db3e80df4822f0e8c523
Manoel Guilherme <fisicorj@gmail.com>
2018-08-13 11:40:24 -0300

    New check for sensu
    new check sensu

---
+++ check_share-windows.ps1
@@ -0,0 +1,21 @@
+﻿$patch = "\\fsb2w01\INFRACORP.N3SO"
+$share_test = Test-Path -Path $patch
+$date = $Date = Get-Date -format "dd-MMM-yyyy HH:mm"
+$message = ""
+$exitCode = -1
+
+If ($share_test -eq $true) {
+
+$message = $patch + " Share check successful  " + $date
+$exitcode = "0"
+
+}
+else {
+
+$message = $patch + " Share not respond  "  + $date
+$exitcode = "2"
+
+}
+
+Write-Output $message
+exit $exitCode
